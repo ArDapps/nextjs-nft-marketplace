7a7d4fc262cc31ca340fd2eff0ab2b281ebf86e2
Jason J <99030878+smartdevb16@users.noreply.github.com>
2022-02-16 15:15:24 +0200

    .

--- components/index/paragraph1.js
+++ components/index/paragraph1.js
@@ -4,11 +4,11 @@ export default function Paragraph1() {
     const description = "OpenSea is the world's first and largest NFT marketplace"
     
     return (
-        <div className='flex flex-row'>
+        <div className='flex flex-col space-y-8 md:flex-row md:space-y-0 mx-8 sm:mx-16 lg:mx-[9vw]'>
             {/* left element */}
-            <div className='relative'>
-                <div className='absolute top-1/2 -translate-y-1/2 sm:top-[12vw] sm:translate-y-0 left-[10vw] w-[80vw] md:w-[50vw] lg:w-[40vw] flex flex-col text-white dark:text-gray-800'>
-                    <h1 className=' text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold leading-tight'>{title}</h1>
+            <div className='relative mt-8 sm:mt-16 lg:mt-[9vw]'>
+                <div className='md:absolute md:top-[12vw] md:left-0 md:w-[50vw] flex flex-col items-center md:items-start text-white dark:text-gray-800'>
+                    <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold leading-tight'>{title}</h1>
                     <p className=' text-base leading-tight mt-2 sm:mt-6'>{description}</p>
                     <div className='flex-1 flex flex-row gap-8 mt-[4vw]'>
                         <div className=''>
@@ -23,7 +23,7 @@ export default function Paragraph1() {
 
             {/* right element : background image */}
             <div className='flex-1 overflow-hidden'>
-                <img src="/assets/svg/bg-ethereum-nft.svg" alt="BgEthereumNFT" className='max-w-full h-auto ml-[22vw]'></img>
+                <img src="/assets/svg/bg-ethereum-nft.svg" alt="BgEthereumNFT" className='sm:max-w-full h-auto ml-0 md:ml-[22vw]'></img>
             </div>
         </div>
     )
