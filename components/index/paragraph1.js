d3490501e47555d5c8bf0673cd07fba570545542
Jason J <99030878+smartdevb16@users.noreply.github.com>
2022-02-16 17:26:38 +0200

    .

--- components/index/paragraph1.js
+++ components/index/paragraph1.js
@@ -4,26 +4,24 @@ export default function Paragraph1() {
     const description = "OpenSea is the world's first and largest NFT marketplace"
     
     return (
-        <div className='flex flex-col space-y-8 md:flex-row md:space-y-0 mx-8 sm:mx-16 '>
+        <div className='flex flex-col space-y-8 md:flex-row md:space-y-0 md:items-center m-8 sm:m-16 '>
             {/* left element */}
-            <div className='relative mt-8 sm:mt-16'>
-                <div className='md:absolute md:top-1/2 md:-translate-y-1/2 md:left-0 md:w-[50vw] lg:w-[40vw] flex flex-col items-center md:items-start text-white dark:text-gray-800'>
-                    <h1 className='text-2xl sm:text-3xl md:text-4xl xl:text-5xl font-bold leading-tight'>{title}</h1>
-                    <p className=' text-base leading-tight mt-2 sm:mt-6'>{description}</p>
-                    <div className='flex-1 flex flex-row gap-8 mt-[4vw]'>
-                        <div className=''>
-                            <button className='rounded-full bg-gradient-to-b from-[#3461FF] to-[#8454EB] text-white text-xs sm:text-base px-6 sm:px-10 py-1.5 sm:py-2 shadow-lg'>Collection</button>
-                        </div>
-                        <div className=''>
-                            <button className='rounded-full border text-xs sm:text-base px-6 sm:px-10 py-1.5 sm:py-2 dark:border-gray-300 shadow-lg'>Create NFT</button>
-                        </div>
+            <div className='flex-1 flex flex-col items-center md:items-start'>
+                <h1 className='text-white dark:text-gray-800 text-2xl sm:text-3xl md:text-4xl xl:text-5xl font-bold leading-tight'>{title}</h1>
+                <p className='text-white dark:text-gray-800 text-base leading-tight mt-2 sm:mt-6'>{description}</p>
+                <div className='flex-1 flex flex-row gap-4 mt-[4vw]'>
+                    <div className=''>
+                        <button className='rounded-full bg-gradient-to-b from-[#3461FF] to-[#8454EB] text-white text-xs sm:text-base px-6 sm:px-10 py-1.5 sm:py-2 shadow-lg'>Collection</button>
+                    </div>
+                    <div className=''>
+                        <button className='rounded-full border text-white dark:text-gray-800 text-xs sm:text-base px-6 sm:px-10 py-1.5 sm:py-2 dark:border-gray-300 shadow-lg'>Create NFT</button>
                     </div>
                 </div>
             </div>
 
             {/* right element : background image */}
-            <div className='flex-1 overflow-hidden'>
-                <img src="/assets/svg/bg-ethereum-nft.svg" alt="BgEthereumNFT" className='sm:max-w-full h-auto ml-0 md:ml-[22vw]'></img>
+            <div className='flex-1 flex items-center'>
+                <img src="/assets/svg/bg-ethereum-nft.svg" alt="BgEthereumNFT" className='w-full h-auto object-cover'></img>
             </div>
         </div>
     )
